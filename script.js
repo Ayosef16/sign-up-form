d059d1ea388d6fc3c072ac5771e5597d3c24198a
Ayosef <syrivera@uc.cl>
2023-04-18 12:53:33 -0400

    Fix input border and shadows

---
+++ script.js
@@ -0,0 +1,8 @@
+const userPassword = document.querySelector('.user-password');
+const confirmPassword = document.querySelector('.confirm-password');
+const error = document.querySelector('.error');
+const form = document.getElementById('form');
+
+// form.addEventListener(submit, (e) => {
+//     e.preventDefault()
+// })
