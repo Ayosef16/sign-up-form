3550a45a8cc210db6c03e40f7f85dff2e3017e03
Ayosef <syrivera@uc.cl>
2023-04-19 09:36:01 -0400

    Add a red border if passwords does not match

--- script.js
+++ script.js
@@ -6,7 +6,13 @@ const form = document.getElementById('form');
 
 form.addEventListener("submit", (e) => {
     if(userPassword.value !== confirmPassword.value) {
-        error.textContent = "*Password doesn't match, try again"
+        error.textContent = "*Password doesn't match, try again";
+        userPassword.classList.add('red-border');
+        confirmPassword.classList.add('red-border');
         e.preventDefault();
+    }
+    else{
+        userPassword.classList.remove('red-border');
+        confirmPassword.classList.remove('red-border');
     }
 })
