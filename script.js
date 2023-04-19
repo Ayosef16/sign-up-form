70b2443637aaabcd83aed229698d55ea7c9c51f5
Ayosef <syrivera@uc.cl>
2023-04-19 09:15:06 -0400

    Add an error message if password doesn't match

--- script.js
+++ script.js
@@ -1,8 +1,12 @@
-const userPassword = document.querySelector('.user-password');
-const confirmPassword = document.querySelector('.confirm-password');
+const userPassword = document.querySelector('#user-password');
+const confirmPassword = document.querySelector('#confirm-password');
 const error = document.querySelector('.error');
 const form = document.getElementById('form');
 
-// form.addEventListener(submit, (e) => {
-//     e.preventDefault()
-// })
+
+form.addEventListener("submit", (e) => {
+    if(userPassword.value !== confirmPassword.value) {
+        error.textContent = "*Password doesn't match, try again"
+        e.preventDefault();
+    }
+})
